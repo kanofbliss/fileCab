167a7e247534bbdea224d340123e0fff930789fe
$onny <hgpmac157@Sonnys-MacBook-Air.local>
2019-12-04 18:26:22 -0800

    created Doc model and preformed rake

---
+++ db/migrate/20191205022252_create_docs.rb
@@ -0,0 +1,11 @@
+class CreateDocs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :docs do |t|
+      t.string :title
+      t.string :content
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
