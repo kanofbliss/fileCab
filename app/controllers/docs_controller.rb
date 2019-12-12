b91f2003e021a917a893dce97657d121f53f003b
$onny <hgpmac157@Sonnys-MacBook-Air.local>
2019-12-11 18:09:22 -0800

    edit docCont, added fix, destroy, and create

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,8 +1,12 @@
 class DocsController < ApplicationController
+before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
     def index
+      @docs = Doc.all.order("created_at DESC")
     end
 
     def show
+
     end
 
     def new
@@ -23,17 +27,25 @@ class DocsController < ApplicationController
     end
 
     def update
+      if @doc = Doc.new(doc_params)
+        redirect_to @doc
+      else
+        render "edit"
+      end
     end
 
     def destroy
+      @doc.destroy
+      redirect_to docs_path
     end
 
     private
-     def find_docs
+     def find_doc
+       @doc = Doc.find(params[:id])
      end
 
-     def docs_params
-       params.require(:docs).permit(:title, :content)
+     def doc_params
+       params.require(:doc).permit(:title, :content)
      end
 
-  end
+   end
